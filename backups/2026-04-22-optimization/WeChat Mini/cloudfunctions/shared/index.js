8d3905b2d68e7dcd86c4f46c1f6113c6b63d0271
Missonce <Emmiby@icloud.com>
2026-05-18 00:32:50 +0800

    Delete backups directory

--- backups/2026-04-22-optimization/WeChat Mini/cloudfunctions/shared/index.js
+++
@@ -1,9 +0,0 @@
-const CloudFunctionPerformance = require('./performance')
-const CloudCache = require('./cache')
-const constants = require('./constants')
-
-module.exports = {
-  CloudFunctionPerformance,
-  CloudCache,
-  ...constants
-}
