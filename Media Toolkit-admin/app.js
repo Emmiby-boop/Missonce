267fdab0cf235ae5ba73fbd005f38198c0878e87
Missonce <missonce@example.com>
2026-06-17 22:39:30 +0800

    fix: CORS allow x-api-key header for admin auth

--- Media Toolkit-admin/app.js
+++ Media Toolkit-admin/app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
