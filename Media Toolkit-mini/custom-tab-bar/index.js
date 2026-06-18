1362789defdeb9ba7769d13c11ff1f698fb2a6df
Missonce <missonce@example.com>
2026-06-18 11:56:41 +0800

    fix: 自定义tabBar去掉pageLifetimes，简化切换逻辑避免索引错乱

--- Media Toolkit-mini/custom-tab-bar/index.js
+++ Media Toolkit-mini/custom-tab-bar/index.js
@@ -1,6 +1,5 @@
 var config = require('../utils/config');
 
-// tabBar 配置
 var allTabs = [
   { pagePath: 'pages/index/index', text: '首页', iconPath: '/images/home.png', selectedIconPath: '/images/home-active.png' },
   { pagePath: 'pages/trending/trending', text: '热门', iconPath: '/images/trending.png', selectedIconPath: '/images/trending-active.png' },
@@ -20,17 +19,9 @@ Component({
     },
   },
 
-  pageLifetimes: {
-    show: function() {
-      this._loadTabs();
-      this._updateSelected();
-    },
-  },
-
   methods: {
     _loadTabs: function() {
       var self = this;
-      // 每次都从服务器拉取最新配置
       wx.request({
         url: config.baseURL + '/api/page-config',
         success: function(res) {
@@ -43,7 +34,6 @@ Component({
           }
         },
         fail: function() {
-          // 失败时用本地缓存
           var pageConfig = wx.getStorageSync('page_config') || {};
           self._filterTabs(pageConfig);
         }
@@ -53,30 +43,14 @@ Component({
     _filterTabs: function(pageConfig) {
       var filtered = allTabs.filter(function(tab) {
         var page = pageConfig[tab.pagePath];
-        // 默认启用，除非明确禁用
         return !page || page.enabled !== false;
       });
       this.setData({ tabs: filtered });
-      this._updateSelected();
-    },
-
-    _updateSelected: function() {
-      var pages = getCurrentPages();
-      var current = pages[pages.length - 1];
-      var currentPath = current ? current.route : '';
-      var tabs = this.data.tabs;
-      for (var i = 0; i < tabs.length; i++) {
-        if (tabs[i].pagePath === currentPath) {
-          this.setData({ selected: i });
-          return;
-        }
-      }
     },
 
     switchTab: function(e) {
       var index = e.currentTarget.dataset.index;
       var path = e.currentTarget.dataset.path;
-      if (index === this.data.selected) return;
       wx.switchTab({ url: '/' + path });
     },
   },
