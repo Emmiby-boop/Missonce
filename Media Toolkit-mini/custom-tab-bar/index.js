a7062c06d0c0a03ecca25099fa609fa582f3c624
Missonce <missonce@example.com>
2026-06-18 12:23:09 +0800

    rename: 热门页改名为发现

--- Media Toolkit-mini/custom-tab-bar/index.js
+++ Media Toolkit-mini/custom-tab-bar/index.js
@@ -2,7 +2,7 @@ var config = require('../utils/config');
 
 var allTabs = [
   { pagePath: 'pages/index/index', text: '首页', iconPath: '/images/home.png', selectedIconPath: '/images/home-active.png' },
-  { pagePath: 'pages/trending/trending', text: '热门', iconPath: '/images/trending.png', selectedIconPath: '/images/trending-active.png' },
+  { pagePath: 'pages/trending/trending', text: '发现', iconPath: '/images/trending.png', selectedIconPath: '/images/trending-active.png' },
   { pagePath: 'pages/history/history', text: '历史', iconPath: '/images/history.png', selectedIconPath: '/images/history-active.png' },
   { pagePath: 'pages/mine/mine', text: '我的', iconPath: '/images/profile.png', selectedIconPath: '/images/profile-active.png' },
 ];
